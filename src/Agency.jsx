a0be80dfb78817839dabb94a30014bdfd137ee13
Carlos Alejandro <27aniconsol@gmail.com>
2024-10-12 16:30:40 -0500

    commit

--- src/Agency.jsx
+++ src/Agency.jsx
@@ -14,61 +14,40 @@ function Agency() {
     const videos = [splash1, splash2, splash3, splash4];
 
     useEffect(() => {
-        const isMobile = window.innerWidth < 768; // Verificar si el dispositivo es móvil
+        const fadeInTimeout = setTimeout(() => {
+            setLoading(true);
+        }, 0);
 
-        if (isMobile) {
-            // Si es móvil, seleccionar un video aleatorio
-            const randomVideoIndex = Math.floor(Math.random() * videos.length);
-            const randomVideo = videos[randomVideoIndex];
+        const intervals = [
+            setTimeout(() => setActiveVideo(1), 0),
+            setTimeout(() => {
+                if (videoRefs[0].current) videoRefs[0].current.pause();
+                setActiveVideo(2);
+            }, 2000),
+            setTimeout(() => {
+                if (videoRefs[1].current) videoRefs[1].current.pause();
+                setActiveVideo(3);
+            }, 4500),
+            setTimeout(() => {
+                if (videoRefs[2].current) videoRefs[2].current.pause();
+                setActiveVideo(4);
+            }, 5500),
+            setTimeout(() => {
+                if (videoRefs[3].current) videoRefs[3].current.pause();
+                setFadingOut(true);
+            }, 8500),
+            setTimeout(() => {
+                setLoading(false);
+            }, 12000)
+        ];
 
-            const videoElement = document.createElement('video');
-            videoElement.src = randomVideo;
-            videoElement.muted = true;
-            videoElement.play();
-
-            const timeout = setTimeout(() => {
-                videoElement.pause();
-                videoElement.remove(); // Eliminar el elemento del DOM después de 10 segundos
-                setFadingOut(true); // Iniciar el fade-out
-                setLoading(false); // Finalizar el splash
-            }, 10000); // 10 segundos
-
-            return () => clearTimeout(timeout); // Limpiar timeout al desmontar
-        } else {
-            // Si no es móvil, seguir con la lógica de videos secuenciales
-            const fadeInTimeout = setTimeout(() => {
-                setLoading(true);
-            }, 0);
-
-            const intervals = [
-                setTimeout(() => setActiveVideo(1), 0),
-                setTimeout(() => {
-                    if (videoRefs[0].current) videoRefs[0].current.pause();
-                    setActiveVideo(2);
-                }, 2000),
-                setTimeout(() => {
-                    if (videoRefs[1].current) videoRefs[1].current.pause();
-                    setActiveVideo(3);
-                }, 4500),
-                setTimeout(() => {
-                    if (videoRefs[2].current) videoRefs[2].current.pause();
-                    setActiveVideo(4);
-                }, 5500),
-                setTimeout(() => {
-                    if (videoRefs[3].current) videoRefs[3].current.pause();
-                    setFadingOut(true);
-                }, 8500),
-                setTimeout(() => {
-                    setLoading(false);
-                }, 12000)
-            ];
-
-            return () => {
-                clearTimeout(fadeInTimeout);
-                intervals.forEach(clearTimeout); // Limpiar todos los temporizadores
-            };
-        }
+        return () => {
+            clearTimeout(fadeInTimeout);
+            intervals.forEach(clearTimeout);
+        };
     }, []);
+
+    
 
     if (loading) {
         return (
